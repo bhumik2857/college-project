2387f0c71c489bccce5bf00c58d1ac955c7d0c80
Bhumika Gupta <168001587+bhumik2857@users.noreply.github.com>
2026-05-01 11:31:09 +0530

    Add cgpa field to user schema
    Added 'cgpa' field with default value to user schema.

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  role: String
+  role: String,
+  cgpa: { type: Number, default: 0 }   // ✅ SAFE ADDITION
 });
 
 module.exports = mongoose.model("User", userSchema);
